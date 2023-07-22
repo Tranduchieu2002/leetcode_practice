29bad93edb015d049bc4871e082b11c1ac47a2dd
Trần Đức Hiếu <73355495+Tranduchieu2002@users.noreply.github.com>
2023-07-23 02:35:06 +0700

    Time: 2290 ms (5.00%), Space: 51.3 MB (8.82%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,18 +1,18 @@
 class Solution {
     vector<int> memo;
 public:
-    bool solve(int i, vector<int>& nums) {
-        if (i == nums.size() - 1) return true;
+    bool solve(int i,int n, vector<int>& nums) {
+        if (i == n - 1) return true;
         if (nums[i] == 0) return false;
         if (memo[i] != -1) return memo[i];
         for (int idx = 1; idx <= nums[i]; ++idx) {
-            if (i + idx < nums.size() && solve(i + idx, nums)) return memo[i] = true;
+            if (i + idx < n && solve(i + idx, n, nums)) return memo[i] = true;
         }
         return memo[i] = false;
     }
     
     bool canJump(vector<int>& nums) {
         memo.resize(nums.size(), -1);
-        return solve(0, nums);
+        return solve(0, nums.size(), nums);
     }
 };
