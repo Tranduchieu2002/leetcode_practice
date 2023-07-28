8ff0d6c611947a66be6be6fb4267efbdd0a21d1f
Trần Đức Hiếu <73355495+Tranduchieu2002@users.noreply.github.com>
2023-07-29 00:35:12 +0700

    Time: 7 ms (43.71%), Space: 7.7 MB (29.30%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int n;
+    vector<int> nums;
+    bool PredictTheWinner(vector<int>& nums) {
+        if(nums.size() == 1) return true;
+        n = nums.size();
+        this->nums = nums;
+        vector<vector<int>> dp(n , vector<int>(n, -1));
+        int sum = 0;
+
+        for(auto val : nums) {
+            sum += val;
+        }
+        
+        solve(0, n - 1, dp);
+
+        for(auto v1 : dp) {
+            for(auto v2: v1) {
+                cout << v2 << "  ";
+            }
+            cout << endl;
+        }
+        return dp[0][n - 1] >= 0;
+        
+    }
+    int solve(int i, int j, vector<vector<int>> &dp) {
+        if(i > j) return 0;
+
+        // if(i == j) return nums[i];
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        int takeHead = nums[i] - solve(i + 1, j, dp);
+
+        int takeTail = nums[j] - solve(i, j - 1, dp);
+            
+        return dp[i][j] = max(takeHead, takeTail);            
+    }
+};
