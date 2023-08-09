de8ed10652d7076e4f2b6ed818b611cd8dad212b
Trần Đức Hiếu <73355495+Tranduchieu2002@users.noreply.github.com>
2023-08-10 00:49:15 +0700

    Time: 1021 ms (91.43%), Space: 64 MB (76.66%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.py
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.py
@@ -3,11 +3,9 @@ class Solution:
         envelopes.sort(key=lambda x: (x[0], -x[1]))
         print(envelopes)
         dp = []
-        ans = 0
         for w, h in envelopes:
             if not dp or dp[-1] < h:
                 dp.append(h)
-                ans += 1
             else:
                 idx = bisect_left(dp, h)
                 dp[idx] = h
