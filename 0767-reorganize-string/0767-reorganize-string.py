0cb1689b26bd7cd5d82147f3f57282beb93c4638
Trần Đức Hiếu <73355495+Tranduchieu2002@users.noreply.github.com>
2023-08-27 03:44:01 +0700

    Time: 43 ms (49.67%), Space: 16.5 MB (11.11%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -2,29 +2,27 @@ from collections import Counter
 
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        mp = Counter(s)
-        mp = sorted(mp.items(), key=lambda pair: pair[1], reverse=True)
+        counter = Counter(s)
         n = len(s)
-        if mp[0][1] > (n + 1) // 2:
+        pq = [(-freq, char) for char, freq in counter.items()]
+        heapq.heapify(pq)
+        print(pq)
+        if pq[0][0] > (n + 1) // 2:
             return ""
         ans = ["" for _ in range(n)]
-        m = len(mp)
-        i, j = 0, 0
-        while i < len(s):
-            while j < m and mp[j][1] == 0:
-                j += 1
-            if j == m:
-                break
-            ans[i] = mp[j][0]
-            mp[j] = (mp[j][0], mp[j][1] - 1)
-            i += 2
-        i = 1
-        while i < len(s):
-            while j < m and mp[j][1] == 0:
-                j += 1
-            if j == m:
-                break
-            ans[i] = mp[j][0]
-            mp[j] = (mp[j][0], mp[j][1] - 1)
-            i += 2
+        while len(pq) >= 2:
+            (f1, c1) = heapq.heappop(pq)
+            (f2, c2) = heapq.heappop(pq)
+            
+            if f1 + 1 < 0:
+                heapq.heappush(pq, (f1 + 1, c1))
+            if f2 + 1 < 0:
+                heapq.heappush(pq, (f2 + 1, c2))
+            ans.append(c1 + c2)
+        if len(pq) == 1:
+            (f1, c1) = heapq.heappop(pq)
+            if f1 != -1:
+                return ""
+            else:
+                ans.append(c1)
         return "".join(ans)
