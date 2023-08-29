7c90327e83f28b0ef075cbc2de7e56521e1add13
Trần Đức Hiếu <73355495+Tranduchieu2002@users.noreply.github.com>
2023-08-30 01:55:43 +0700

    Time: 113 ms (83.74%), Space: 17.4 MB (73.49%) - LeetHub

---
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.py
@@ -0,0 +1,12 @@
+class Solution:
+    def bestClosingTime(self, customers: str) -> int:
+        n = len(customers)
+        ans = 0
+        cur_p = p = customers.count('Y')
+        for i in range(n):
+            c = customers[i]
+            cur_p += 1 if c == 'N' else -1
+            if cur_p < p:
+                p = cur_p
+                ans = i + 1
+        return ans
