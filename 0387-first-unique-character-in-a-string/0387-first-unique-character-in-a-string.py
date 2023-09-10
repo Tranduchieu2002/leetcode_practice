88ad7ad950c7ccafe7df9a94190f4e84c9ad5608
Trần Đức Hiếu <73355495+Tranduchieu2002@users.noreply.github.com>
2023-09-10 10:06:05 +0700

    Time: 42 ms (99.86%), Space: 16.5 MB (47.51%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -13,11 +13,14 @@ class Solution:
         #             break
         # return  dq[0] if dq else -1
         
-        # chars = 'abcdefghijklmnopqrstuvwxyz'
-        for i, c in enumerate(s):
+        chars = 'abcdefghijklmnopqrstuvwxyz'
+        min_ans = float('inf')
+        for c in (chars):
             first_find_i = s.find(c)
+            if first_find_i == -1:
+                continue
             right_find_i = s.rfind(c)
             if first_find_i == right_find_i:
-                return i
-        return  -1
+                min_ans = min(min_ans, first_find_i)
+        return -1 if min_ans == float('inf') else min_ans
             
