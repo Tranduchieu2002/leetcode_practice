ecec38fc7676fa804b959f2817c8970135bd5b41
Trần Đức Hiếu <73355495+Tranduchieu2002@users.noreply.github.com>
2023-09-11 13:09:00 +0700

    Time: 66 ms (93.86%), Space: 16.5 MB (64.84%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
@@ -1,24 +1,13 @@
 class Solution:
     def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
-        mp = {}
-        n = len(groupSizes)
-
-        for i in range(n):
-            size = groupSizes[i]
-            if size not in mp:
-                mp[size] = []
-            mp[size].append(i)
+        groups = {}
+        for i, size in enumerate(groupSizes):
+            groups.setdefault(size, []).append(i)
 
         ans = []
 
-        for key in mp:
-            totalNumberOfGroup = len(mp[key])
-            perNumInGroup = totalNumberOfGroup // key
-            temp = []
-
-            for i in range(0, totalNumberOfGroup, key):
-                temp.append(mp[key][i:i+key])
-
-            ans.extend(temp)
+        for key, members in groups.items():
+            for i in range(0, len(members), key):
+                ans.append(members[i:i + key])
 
         return ans
