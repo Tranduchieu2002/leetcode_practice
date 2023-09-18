6828ec5421605039b1ae4e7094caf4e73c8793c2
Trần Đức Hiếu <73355495+Tranduchieu2002@users.noreply.github.com>
2023-09-18 23:45:42 +0700

    Time: 145 ms (88.75%), Space: 21.6 MB (38.13%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div847-shortest-path-visiting-all-nodes/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div847-shortest-path-visiting-all-nodes.py
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div847-shortest-path-visiting-all-nodes/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div847-shortest-path-visiting-all-nodes.py
@@ -1,21 +1,24 @@
 class Solution:
     def shortestPathLength(self, graph: List[List[int]]) -> int:
         n = len(graph)
-        q , seen = deque(), set()
+        q , seen = [], set()
 #       q including (cur node , dist, mask) and seen including (node, mask)
         maskAllPaths = (1 << n) - 1
+        dist = 0
         for i in range(n):
-            q.append((i, 0, 1 << i))
+            q.append((i, 1 << i))
+            seen.add((i, 1 << i))
         while q:
-            cur_node, dist, mask = q.popleft()
-            if mask == maskAllPaths:
-                return dist
-            
-            for neighbor in (graph[cur_node]):
-                next_mask = mask | ( 1 << neighbor )
-                if (neighbor, next_mask) not in seen:
-                    seen.add((neighbor, next_mask))
-                    q.append((neighbor, dist + 1, next_mask))
-            
+            dist += 1
+            new_state_q = []
+            for cur_node, mask in q:
+                for neighbor in (graph[cur_node]):
+                    next_mask = mask | ( 1 << neighbor)
+                    if next_mask == maskAllPaths:
+                        return dist
+                    if (neighbor, next_mask) not in seen:
+                        seen.add((neighbor, next_mask))
+                        new_state_q.append((neighbor, next_mask))
+            q = new_state_q
          # Node with mask off thiss node 
         return 0
