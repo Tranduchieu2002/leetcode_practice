3383081bef8ebedd77975577b9fef43793e67859
Trần Đức Hiếu <73355495+Tranduchieu2002@users.noreply.github.com>
2023-10-03 15:13:24 +0700

    Time: 48 ms (78.15%), Space: 41.7 MB (72.70%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.js
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.js
@@ -4,10 +4,10 @@
  */
 var numIdenticalPairs = function(nums) {
     let ans = 0;
-    const cnt = Array(101).fill(0);
-    for(const val of nums) {
-        ans += cnt[val];
-        ++cnt[val];
+    for(let i = 0; i < nums.length; ++i) {
+        for(let j = i + 1; j < nums.length; ++j) {
+            if(nums[i] === nums[j]) ++ans;
+        }
     }
     return ans;
 };
