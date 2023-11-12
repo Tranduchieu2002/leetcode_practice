16a75defe9cd12ca71a9a408b86ec6921aeb54a7
Trần Đức Hiếu <73355495+Tranduchieu2002@users.noreply.github.com>
2023-11-12 18:27:30 +0700

    Time: 2736 ms (12.29%), Space: 36.8 MB (85.36%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.py
+++ 0815-bus-routes/0815-bus-routes.py
@@ -1,37 +1,33 @@
-from collections import defaultdict
-
 class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
-        if source == target:
-            return 0
-
         adj = defaultdict(list)
         n = len(routes)
-
-        # Build adjacency list
         for i in range(n):
-            for stop in routes[i]:
-                adj[stop].append(i)
-
+            for route  in routes[i]:
+                adj[route].append(i)
+        
         q = [source]
-        visited = [False] * n  # Use n instead of (n + 1)
+        visited = [False] * (n + 1)
         ans = 0
-
+        
         while q:
             size = len(q)
             while size > 0:
-                current_stop = q.pop(0)
-                for route in adj[current_stop]:
-                    if visited[route]:
+                top = q.pop(0)
+                
+                if top == target:
+                    return ans
+                
+                for nei in adj[top]:
+                    
+                    if (visited[nei]):
                         continue
-
-                    visited[route] = True
-                    for next_stop in routes[route]:
-                        if next_stop == target:
-                            return ans + 1
-                        q.append(next_stop)
-
+                    visited[nei] = True
+                    
+                    for route in routes[nei]:
+                        q.append(route)
                 size -= 1
+            
             ans += 1
-
-        return -1  # No valid route found
+        return -1
+        
