c18606c7b3ca17ced1f80779654783f7ef8518f6
Trần Đức Hiếu <73355495+Tranduchieu2002@users.noreply.github.com>
2023-11-27 22:58:22 +0700

    Time: 309 ms (90.69%), Space: 16.2 MB (91.46%) - LeetHub

---
+++ 0935-knight-dialer/0935-knight-dialer.py
@@ -0,0 +1,30 @@
+class Solution:
+    MOD = 10**9 + 7
+
+    def knightDialer(self, n: int) -> int:
+        cur_pos = [1] * 10
+
+        # Loop through the number of jumps required
+        for jump in range(2, n + 1):
+            # Create a new list to store the updated positions after each jump
+            new_pos = [0] * 10
+
+            # Calculate the new positions based on the valid knight moves
+            new_pos[0] = (cur_pos[6] + cur_pos[4]) % self.MOD
+            new_pos[1] = (cur_pos[6] + cur_pos[8]) % self.MOD
+            new_pos[2] = (cur_pos[7] + cur_pos[9]) % self.MOD
+            new_pos[3] = (cur_pos[4] + cur_pos[8]) % self.MOD
+            new_pos[4] = (cur_pos[0] + cur_pos[3] + cur_pos[9]) % self.MOD
+            new_pos[5] = 0  # Knight cannot move to position 5
+            new_pos[6] = (cur_pos[0] + cur_pos[1] + cur_pos[7]) % self.MOD
+            new_pos[7] = (cur_pos[2] + cur_pos[6]) % self.MOD
+            new_pos[8] = (cur_pos[1] + cur_pos[3]) % self.MOD
+            new_pos[9] = (cur_pos[2] + cur_pos[4]) % self.MOD
+
+            # Update the current positions list for the next iteration
+            cur_pos = new_pos
+
+        # Calculate the total count of distinct phone numbers
+        total_count = sum(cur_pos) % self.MOD
+
+        return total_count
