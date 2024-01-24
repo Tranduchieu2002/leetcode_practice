7e02c835f30b2e0fda9f022332328f7f30d646fc
Trần Đức Hiếu <73355495+Tranduchieu2002@users.noreply.github.com>
2024-01-24 13:47:42 +0700

    Time: 321 ms (100.00%), Space: 43.5 MB (98.67%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
@@ -10,12 +10,13 @@ class Solution:
         # => dfs and count number of element
         if root is None:
             return 0
+        
         cnt ^= 1 << (root.val - 1)
         
         res = self.pseudoPalindromicPaths(root.left, cnt) + self.pseudoPalindromicPaths(root.right, cnt)
         
         # in postorder when root.left = root.right meaning leftnode is root
         if root.left == root.right:
-            if cnt & cnt - 1 == 0:
+            if cnt.bit_count() < 2:
                 res += 1
         return res
