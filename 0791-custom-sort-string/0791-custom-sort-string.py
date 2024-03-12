03faa47820cd4cc7e108d3a0f4d98de2b7417378
Trần Đức Hiếu <73355495+Tranduchieu2002@users.noreply.github.com>
2024-03-12 07:56:07 +0700

    Time: 34 ms (67.95%), Space: 16.8 MB (14.98%) - LeetHub

---
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -0,0 +1,13 @@
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        result = ""
+        mp = {}
+        for char in s:
+            mp[char] = mp.get(char, 0) + 1
+        for char in order:
+            if char in mp:
+                result += char * mp[char]
+                del mp[char]
+        for char, count in mp.items():
+            result += char * count
+        return result
