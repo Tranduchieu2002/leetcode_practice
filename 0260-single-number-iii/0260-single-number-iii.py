3cddab1e7f4894dd8ced26cb0c869a2d371aa03f
Trần Đức Hiếu <73355495+Tranduchieu2002@users.noreply.github.com>
2024-05-31 18:45:40 +0700

    Time: 63 ms (31.43%), Space: 18.5 MB (76.83%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        ans = [0,0]
+        temp = 0
+        
+        for val in range(len(nums)):
+            temp ^= nums[val]
+        flag = 0
+        while temp >> flag & 1 == 0:
+            flag += 1
+        for num in nums:
+            print(num >> flag & 1, num)
+            if (num >> flag & 1) == 1:
+                ans[0] ^= num
+            else:
+                ans[1] ^= num
+        return ans
