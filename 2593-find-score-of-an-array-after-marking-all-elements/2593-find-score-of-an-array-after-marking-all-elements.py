7e1200fa8341e48c939dfe79e3c732082be73110
Trần Đức Hiếu <73355495+Tranduchieu2002@users.noreply.github.com>
2024-12-13 15:36:09 +0700

    Time: 227 ms (96.73%), Space: 31.9 MB (97.75%) - LeetHub

--- 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.py
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.py
@@ -1,21 +1,27 @@
 class Solution:
-    def findScore(self, nums):
+    def findScore(self, nums: List[int]) -> int:
+        N = 10 ** 6
+        n = len(nums)
+        
+        # This is chosen as 20 because shifting nums[i] << 20 + i creates a unique encoding.
+        BASE_MARK = 20
+        nthI = [0] * n
+        mark = [False] * n
+        for i, val in enumerate(nums):
+            nthI[i] = ((val << BASE_MARK) + i)
+        nthI.sort()
         ans = 0
-        marked = [False] * len(nums)
-
-        heap = []
-        for i in range(len(nums)):
-            heapq.heappush(heap, (nums[i], i))
-
-        while heap:
-            number, index = heapq.heappop(heap)
-            if not marked[index]:
-                ans += number
-                marked[index] = True
-                # mark adjacent elements if they exist
-                if index - 1 >= 0:
-                    marked[index - 1] = True
-                if index + 1 < len(nums):
-                    marked[index + 1] = True
-
+        for val in nthI:
+            
+            x = val >> BASE_MARK
+            i = val & ((1 << BASE_MARK) - 1)
+            if mark[i]:
+                continue
+            
+            mark[i] =  True
+            ans += x
+            if i > 0:
+                mark[i - 1] = True
+            if i < n - 1:
+                mark[i + 1] = True
         return ans
